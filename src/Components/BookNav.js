d456ffdfa52409336972ba8764eed48361e0bff9
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-14 23:23:17 +0600

    book part  components added

---
+++ src/Components/BookNav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BookNav = () => {
+    return (
+        <div>
+             <div class="flex items-center justify-between mb-12">
+                <h4 class="mt-2 text-xl font-bold">Book List</h4>
+
+                <div class="flex items-center space-x-4">
+                    <button class="filter-btn active-filter" id="lws-filterAll">All</button>
+                    <button class="filter-btn" id="lws-filterFeatured">Featured</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookNav;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 
+import BookCard from '../Components/BookCard';
+import BookNav from '../Components/BookNav';
+
+
 const Home = () => {
     return (
-       <Navbar></Navbar>
+        <div>
+            <Navbar></Navbar>
+            <main class="py-12 2xl:px-6">
+                <div class="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+                    <div class="order-2 xl:-order-1">
+                        <BookNav></BookNav>
+                        <div class="lws-bookContainer">
+                            <BookCard></BookCard>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
     );
 };
 
