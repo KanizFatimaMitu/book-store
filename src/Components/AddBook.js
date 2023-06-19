bc02dcc23706c946c572453a4e9e1d375fdb4c5b
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-06-19 22:29:41 +0600

    conditional routing done

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AddBook = () => {
     const [name, setName] = useState('')
@@ -6,6 +7,7 @@ const AddBook = () => {
     const [thumbnail, setThumbnail] = useState('')
     const [price, setPrice] = useState('')
     const [featured, setFeatured] = useState('')
+    const navigate = useNavigate();
 
     const reset = () => {
         setName('')
@@ -25,18 +27,24 @@ const AddBook = () => {
             featured
         }
         console.log(data)
-        fetch('http://localhost:5000/books',{
+        fetch('http://localhost:5000/books', {
             method: 'POST',
-            body: JSON.stringify({data}),
+            body: JSON.stringify({ data }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
-              }
+            }
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log('success', data)
-            alert('book added successfully')
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data)
+                alert('book added successfully')
+                if (featured === 'on') {
+                    navigate('/featured')
+                }
+                else {
+                    navigate('/')
+                }
+            })
         reset();
     }
     return (
@@ -45,23 +53,23 @@ const AddBook = () => {
             <form class="book-form" onSubmit={handleAddBook}>
                 <div class="space-y-2">
                     <label for="name">Book Name</label>
-                    <input required class="text-input" type="text" id="input-Bookname" name="name" onChange={(e) => setName(e.target.value) } />
+                    <input required class="text-input" type="text" id="input-Bookname" name="name" onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div class="space-y-2">
                     <label for="category">Author</label>
-                    <input required class="text-input" type="text" id="input-Bookauthor" name="author" onChange={e => setAuthor(e.target.value)}/>
+                    <input required class="text-input" type="text" id="input-Bookauthor" name="author" onChange={e => setAuthor(e.target.value)} />
                 </div>
 
                 <div class="space-y-2">
                     <label for="image">Image Url</label>
-                    <input required class="text-input" type="text" id="input-Bookthumbnail" name="thumbnail" onChange= {e => setThumbnail(e.target.value)} />
+                    <input required class="text-input" type="text" id="input-Bookthumbnail" name="thumbnail" onChange={e => setThumbnail(e.target.value)} />
                 </div>
 
                 <div class="grid grid-cols-2 gap-8 pb-4">
                     <div class="space-y-2">
                         <label for="price">Price</label>
-                        <input required class="text-input" type="number" id="input-Bookprice" name="price" onChange={e => setPrice(e.target.value)}/>
+                        <input required class="text-input" type="number" id="input-Bookprice" name="price" onChange={e => setPrice(e.target.value)} />
                     </div>
 
                     <div class="space-y-2">
@@ -71,7 +79,7 @@ const AddBook = () => {
                 </div>
 
                 <div class="flex items-center">
-                    <input id="input-Bookfeatured" type="checkbox" name="featured" class="w-4 h-4" onChange={e => setFeatured(e.target.value)}/>
+                    <input id="input-Bookfeatured" type="checkbox" name="featured" class="w-4 h-4" onChange={e => setFeatured(e.target.value)} />
                     <label for="featured" class="ml-2 text-sm"> This is a featured book </label>
                 </div>
 
